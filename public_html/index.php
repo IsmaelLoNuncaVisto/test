a2e2c8dec4680481b4806682bb83826a769f76a3
ismael <ismael@lonuncavisto.com>
2023-04-12 09:11:43 +0200

    PHP index funcional

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,59 @@
 <?php
 
-$mysqli = new mysqli("localhost","ismael","ismael");
-
-if($mysqli->connect_errno){
-    echo "Fallo al conectar MariaDB";
-}
-
-echo $mysqli->host_info . "\n";
+        $aniadir=isset($_GET["aniadidura"]);
+        $mostrar=isset($_GET["mostrado"]);
+        $eliminar=isset($_GET["eliminacion"]);
 
 
+        $email=$_GET["email"];
+        $contrasenia=$_GET["contrasenia"];
+        $nombre=$_GET["nombre"];
 
-?>
+        $db_host="localhost";
+        $db_nombre="ismael";
+        $db_user="root";
+        $db_contrasenia="";
+
+        $conexion= mysqli_connect($db_host,$db_user,$db_contrasenia,$db_nombre);
+
+        if(mysqli_connect_errno()){
+            echo "Fallo al conectar con al abase de datos";
+
+            exit();
+
+        }
+
+        mysqli_select_db($conexion,$db_nombre) or die ("No se encuentra la BBDD");
+
+
+        
+        if($aniadir){
+            $aniadirSQL="INSERT INTO usuario(email,contraseña,nombre) VALUES ('$email','$contrasenia','$nombre')";
+            $resultados=mysqli_query($conexion,$aniadirSQL);
+        
+        }
+         if($eliminar){
+            $eliminarSQL="DELETE FROM usuario WHERE email LIKE '$email'";
+            $resultados=mysqli_query($conexion,$eliminarSQL);
+
+        }
+         if($mostrar){
+            $consulta="SELECT * FROM usuario";
+
+            $resultados=mysqli_query($conexion,$consulta);
+            
+            while ($fila=mysqli_fetch_array($resultados, MYSQLI_ASSOC)) {
+                echo "<table><tr><td>";
+                echo $fila['email'] . "</td><td>";
+                echo $fila['contraseña'] . "</td><td>";
+                echo $fila['nombre'] . "</td></tr></table>";
+                echo  "<br>";
+            }
+        }
+
+        
+
+        mysqli_close($conexion);
+        
+
+    ?>
