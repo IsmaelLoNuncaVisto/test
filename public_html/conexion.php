f912bc5b5bf2db5471ac47a323456f36b844d95e
ismael <ismael@lonuncavisto.com>
2023-04-13 18:02:34 +0200

    Validacion Email, Añadido de Try/Catch, EvitarInyeccion

--- public_html/conexion.php
+++ public_html/conexion.php
@@ -24,52 +24,82 @@ class UsoBD
     }
 
     //Se cierra la conexión con la base de datos
-    public function cerrarConexion(){
+    public function cerrarConexion()
+    {
         mysqli_close($this->conexion);
     }
 
     /*Añade usuario a la BD
-    */
-    public function aniadirUsuario($emailUsuario,$psswdUsuario,$nombreUsuario){
-
-        $patronEmail="/@prueba.com$/";
-        $emailValido=preg_match_all($patronEmail,$emailUsuario);
-            if($emailValido){
-                $aniadirSQL="INSERT INTO usuario(email,contrasenia,nombre) VALUES ('$emailUsuario','$psswdUsuario','$nombreUsuario')";
-                if(mysqli_query($this->conexion,$aniadirSQL)){
-                    echo "Usuario añadido";
-                }else{
-                    echo "<script> alert ('Usuario ya existe');</script>";    
-                    exit;
-                }
-            }else{
-                echo "<script> alert ('Email invalido. Estructura del emal----> ejemplousuario@prueba.com')</script>";
+     */
+    public function aniadirUsuario($emailUsuario, $psswdUsuario, $nombreUsuario)
+    {
+        //$emailValido = preg_match_all("/@prueba.com$/", $emailUsuario);
+        $emailValido =filter_var($emailUsuario,FILTER_VALIDATE_EMAIL);
+        try {
+            if ($emailValido==false) {
+                throw new Exception("<script> alert ('Email invalido. Estructura del emal----> ejemplousuario@prueba.com')</script>");
             }
+            $aniadirSQL = "INSERT INTO usuario(email,contrasenia,nombre) VALUES ('$emailUsuario','$psswdUsuario','$nombreUsuario')";
+            if (!mysqli_query($this->conexion, $aniadirSQL)) {
+                throw new mysqli_sql_exception("<script> alert ('Usuario ya existe');</script>");
+            }
+            echo "<script> alert ('Usuario añadido');</script>";
+        } catch (mysqli_sql_exception $me) {
+            echo $me->getMessage();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+
     }
 
     /*Elimina usuario a la BD
-    */
-    public function eliminarUsuario($emailUsuario){
-        $eliminarSQL="DELETE FROM usuario WHERE email LIKE '$emailUsuario'";
-            if(mysqli_query($this->conexion,$eliminarSQL)){
-                echo "Usuario eliminado";
-            }else{
-                echo "<script> alert ('Usuario no existe');</script>";
+     */
+    public function eliminarUsuario($emailUsuario, $psswdUsuario, $nombreUsuario)
+    {
+        //$emailValido = preg_match_all("/@prueba.com$/", $emailUsuario);        
+        $emailValido=filter_var($emailUsuario,FILTER_VALIDATE_EMAIL);
+        try {
+            if (!$emailValido) {
+                throw new Exception("<script> alert ('Email invalido. Estructura del emal----> ejemplousuario@prueba.com')</script>");
             }
+
+            $inyeccionEmail=mysqli_real_escape_string($this->conexion, $emailUsuario);
+
+            $mostrarUsuario="SELECT email FROM usuario WHERE email= '$inyeccionEmail'AND contrasenia='$psswdUsuario' AND nombre='$nombreUsuario'";
+            $existenciaEmail=mysqli_query($this->conexion,$mostrarUsuario);
+
+            while ($fila=mysqli_fetch_assoc($existenciaEmail)) {
+                $email=$fila['email'];
+            }
+
+            if($email==""){
+                throw new mysqli_sql_exception("<script> alert ('Usuario no existe');</script>");
+            } 
+            $inyeccionContrasenia=mysqli_real_escape_string($this->conexion, $psswdUsuario);
+            //$inyeccionNombre=mysqli_real_escape_string($this->conexion, $nombreUsuario);
+            $eliminarSQL = "DELETE FROM usuario WHERE email = '$inyeccionEmail' AND contrasenia='$inyeccionContrasenia' AND nombre='$nombreUsuario' ";
+            mysqli_query($this->conexion, $eliminarSQL);
+            echo "<script> alert ('Usuario eliminado');</script>";
+        } catch (mysqli_sql_exception $me) {
+            echo $me->getMessage();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
     }
 
     /*Muestra a los Usuarios
-        Selecciona todos los Usuarios que hay en la BD
-        Recorre el Array generado
-        Crea una Objeto Usuario por cada Valaor
-        Hace un ECHO pr cada Objeto
-    */
-    public function mostrarUsuarios(){
-        require("usuarioClass.php");
-        $mostrarSQL="SELECT * FROM usuario";
-        $resultado=mysqli_query($this->conexion,$mostrarSQL);
-        while($fila=mysqli_fetch_array($resultado,MYSQLI_ASSOC)){
-            $Usuario=new Usuario($fila['email'],$fila['contrasenia'],$fila['nombre']);
+    Selecciona todos los Usuarios que hay en la BD
+    Recorre el Array generado
+    Crea una Objeto Usuario por cada Valaor
+    Hace un ECHO pr cada Objeto
+     */
+    public function mostrarUsuarios()
+    {
+        require "usuarioClass.php";
+        $mostrarSQL = "SELECT * FROM usuario";
+        $resultado = mysqli_query($this->conexion, $mostrarSQL);
+        while ($fila = mysqli_fetch_array($resultado, MYSQLI_ASSOC)) {
+            $Usuario = new Usuario($fila['email'], $fila['contrasenia'], $fila['nombre']);
             echo $Usuario->getEmailUsuario() . "\t" . $Usuario->getPasswordUsuario() . "\t" . $Usuario->getNombreUsuario() . "<br>";
         }
     }
