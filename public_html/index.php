ec15197b9317c8d9effa16ecd83cbdb23a91e093
ismael <ismael@lonuncavisto.com>
2023-04-13 11:57:07 +0200

    Condicional campos vacíos + Comentarios en Añadir y Eliminar

--- public_html/index.php
+++ public_html/index.php
@@ -15,12 +15,32 @@
         $conexion= new UsoBD();
         $conexion->establecerConexion();
 
+
+        /*
+        Comprueba si están vacíos o no los campos Email, Contraseña y Nombre
+        Si están rellenos ejecuta el método 'aniadirUsuario()'
+        Si están vacíos lanza 'alert()'
+        */
         if($aniadir){
+            if($email!=""&&$contrasenia!=""&&$nombre!=""){
             $conexion->aniadirUsuario($email,$contrasenia,$nombre);
+            }else{
+                echo "<script>alert ('Ha dejado algún espacio en blanco');</script>";
+            }
         }
 
+        /*
+        Comprueba si están vacíos o no los campos Email, Contraseña y Nombre
+        Si están rellenos ejecuta el método 'eliminarUsuario()'
+        Si están vacíos lanza 'alert()'
+        */
          if($eliminar){
-            $conexion->eliminarUsuario($email);
+            if($email!=""&&$contrasenia!=""&&$nombre!=""){
+                $conexion->eliminarUsuario($email);
+                }else{
+                    echo "<script>alert ('Ha dejado algún espacio en blanco');</script>";
+                }
+            
         }
          if($mostrar){
             $conexion->mostrarUsuarios();
