3e982bea6e1517a8683781e120171dc697a06002
ismael <ismael@lonuncavisto.com>
2023-05-08 13:17:03 +0200

    Estructura documento

--- public_html/index.php
+++ public_html/index.php
@@ -5,43 +5,51 @@ require("conexion.php");
 
 use src\Session;
 
-$sesion= new Session();
+$sesion = new Session();
 
-$conexion=new UsoBD;
+$conexion = new UsoBD;
 $conexion->establecerConexion();
 
-if(isset($_POST["login"])){
-    
-    $email=$_POST["email"];
-    $password=$_POST["password"];
+if (isset($_POST["login"])) {
 
-    if($conexion->accesoUsuario($email,$password)){
-        
-        if($conexion->accesoPaginaAdministrador($email)){
-            if(isset($_POST["guardarUsuario"])&&$_POST["guardarUsuario"]=="1"){
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    if ($conexion->accesoUsuario($email, $password)) {
+
+        if ($conexion->accesoPaginaAdministrador($email)) {
+
+            if (isset($_POST["guardarUsuario"]) && $_POST["guardarUsuario"] == "1") {
+
                 $sesion->cookieSession("idSession");
-            }else{
+                
+            } else {
+
                 $sesion->startSession();
-                $sesion->setSession("administrador",$email);
+                $sesion->setSession("administrador", $email);
+
             }
+
             header("Location: https://wwwdes.ismael.lonuncavisto.org/paginaAdministrador1.php");
             exit;
-        }else{
+
+        } else {
+
             $sesion->startSession();
-                $sesion->setSession("noAdministrador",$email);
+            $sesion->setSession("noAdministrador", $email);
             header("Location: https://wwwdes.ismael.lonuncavisto.org/paginaNoAdministrador.php");
             exit;
         }
-    }else{
+    } else {
         echo "Acceso denegado";
     }
-    
 }
 $conexion->cerrarConexion();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -50,50 +58,52 @@ $conexion->cerrarConexion();
     <link rel="stylesheet" href="inicio.css">
 
     <script>
-    function camposVacios(){
-        
-        var email=document.getElementById("email").value;
-        var password=document.getElementById("password").value;
-        if(email==""||password==""){
-            alert("Algunos de los parámetros está vacío");
-            event.preventDefault();
+        function camposVacios() {
+
+            var email = document.getElementById("email").value;
+            var password = document.getElementById("password").value;
+            if (email == "" || password == "") {
+                alert("Algunos de los parámetros está vacío");
+                event.preventDefault();
+            }
+
         }
-        
-    }
     </script>
 
 </head>
+
 <body>
 
 
 
-<form action="" method="post">
-    <ul>
-        <ol>
-            
-            <label for="email">Email:</label>
-            <input type="email" placeholder="ejemplo@prueba.com" name="email" value="" id="email">
-        </ol>
-        <ol>
-            <label for="password">Pasword:</label>
-            <input type="password" name="password" id="password">
-        </ol>
-        <ol>
-            <button type="submit" name="login" onclick="camposVacios()">Login</button>
-        </ol>
-        <ol>
-            <a href="recuperarPassword.php">¿Olvidó su contraseña?</a>
-        </ol>
-        <ol>
-            <a href="crearUsuario.php">Crear una nueva cuenta</a>
-        </ol>
-        <ol>
-            <label for="">
-                <input type="checkbox" name="guardarUsuario" value="1">
-                Recordar mi sesión
-            </label>
-        </ol>
-    </ul>
-</form>
+    <form action="" method="post">
+        <ul>
+            <ol>
+
+                <label for="email">Email:</label>
+                <input type="email" placeholder="ejemplo@prueba.com" name="email" value="" id="email">
+            </ol>
+            <ol>
+                <label for="password">Pasword:</label>
+                <input type="password" name="password" id="password">
+            </ol>
+            <ol>
+                <button type="submit" name="login" onclick="camposVacios()">Login</button>
+            </ol>
+            <ol>
+                <a href="recuperarPassword.php">¿Olvidó su contraseña?</a>
+            </ol>
+            <ol>
+                <a href="crearUsuario.php">Crear una nueva cuenta</a>
+            </ol>
+            <ol>
+                <label for="">
+                    <input type="checkbox" name="guardarUsuario" value="1">
+                    Recordar mi sesión
+                </label>
+            </ol>
+        </ul>
+    </form>
 </body>
+
 </html>
